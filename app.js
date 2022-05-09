481320d45e5f2d8fe66acbaef995787ae7115651
stefan <stefandplac@gmail.com>
2022-05-09 12:43:06 +0100

    make it better

--- app.js
+++ app.js
@@ -70,6 +70,15 @@ app.use('/api/emoticons',returnEmoticons);
 //@ delete chat by chatId
 app.use('/api/deleteChat', deleteChat);
 
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'production') {
+	// npm run build for react app
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+	});
+}
 // Connect to DataBase
 const connectDB = async () => {
 	try {
