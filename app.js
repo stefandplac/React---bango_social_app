50fa252d5d442aec9dc8442c77c074430cf69a99
stefan <stefandplac@gmail.com>
2022-05-16 18:22:17 +0100

    make it better

--- app.js
+++ app.js
@@ -24,12 +24,12 @@ import bodyParser from 'body-parser';
 
 const app = express();
 app.use(cors());
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-  });
+// app.use(function(req, res, next) {
+// 	res.header("Access-Control-Allow-Origin", "*");
+// 	res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// 	next();
+//   });
 
 app.use(fileUpload({
 	createParentPath:true,
